9a631944d0b2824cb6f34a5ffd8288dbabdda306
earsenii <ermolarsen@mail.ru>
2021-10-21 12:45:06 +0300

    ready

--- src/js/input-controller-test.js
+++ src/js/input-controller-test.js
@@ -16,54 +16,64 @@ const actionsB = {
 };
 
 let testBlock = document.querySelector(".test_block");
-let left = 200
-testBlock.style.left= left + "px";
+let left = 200;
+let topCoordinate = 200;
+testBlock.style.left = left + "px";
+testBlock.style.top = topCoordinate + "px";
 
 let controller = new inputController(actionsA, document);
-let action;
 
+let addJumpButton = document.querySelector(".btnAddJump");
+addJumpButton.addEventListener("click", () => {
+  controller.bindActions(actionsB);
+});
 // window.addEventListener("input-controller:action-activated",(e)=>{
 //   console.log(e)
 // })
 window.addEventListener("left", () => {
-  const startMove = setInterval(()=>{
-    left -= 5 
-  testBlock.style.left= left + "px"
-  },30)
-  window.addEventListener("input-controller:action-deactivated",(e)=>{
-    clearInterval(startMove)
-  })
-})
+  const startMove = setInterval(() => {
+    left -= 5;
+    testBlock.style.left = left + "px";
+  }, 30);
+  window.addEventListener("input-controller:action-deactivated", (e) => {
+    clearInterval(startMove);
+  });
+});
 
 window.addEventListener("right", () => {
-  const startMove = setInterval(()=>{
-    left += 5 
-  testBlock.style.left= left + "px"
-  },30)
-  window.addEventListener("input-controller:action-deactivated",(e)=>{
-    clearInterval(startMove)
-  })
-})
+  const startMove = setInterval(() => {
+    left += 5;
+    testBlock.style.left = left + "px";
+  }, 30);
+  window.addEventListener("input-controller:action-deactivated", (e) => {
+    clearInterval(startMove);
+  });
+});
 
 window.addEventListener("up", () => {
-  const startMove = setInterval(()=>{
-    left -= 5 
-  testBlock.style.left= left + "px"
-  },30)
-  window.addEventListener("input-controller:action-deactivated",(e)=>{
-    clearInterval(startMove)
-  })
-})
+  const startMove = setInterval(() => {
+    topCoordinate -= 5;
+    testBlock.style.top = topCoordinate + "px";
+  }, 30);
+  window.addEventListener("input-controller:action-deactivated", (e) => {
+    clearInterval(startMove);
+  });
+});
 
 window.addEventListener("down", () => {
-  const startMove = setInterval(()=>{
-    left -= 5 
-  testBlock.style.left= left + "px"
-  },30)
-  window.addEventListener("input-controller:action-deactivated",(e)=>{
-    clearInterval(startMove)
-  })
-})
+  const startMove = setInterval(() => {
+    topCoordinate += 5;
+    testBlock.style.top = topCoordinate + "px";
+  }, 30);
+  window.addEventListener("input-controller:action-deactivated", (e) => {
+    clearInterval(startMove);
+  });
+});
 
-
-// controller.bindActions(actionsB);
+window.addEventListener("jump", () => {
+  if (testBlock.style.background === "red") {
+    testBlock.style.background = "teal";
+  } else {
+    testBlock.style.background = "red";
+  }
+});
--- src/js/input-controller.js
+++ src/js/input-controller.js
@@ -7,53 +7,91 @@ class inputController {
 
   constructor(actionsToBind, target) {
     this.actionsToBind = actionsToBind;
-    this.target = target; 
-    this.target.addEventListener("keydown", (e) => {
-      for(let action in this.actionsToBind){
-        if(this.isActionActive(action)){
-          return
-        }
-        if(this.actionsToBind[action].keys.includes(e.keyCode)){
-          this.enableAction(action)
-        }
-      }
+    this.target = target;
+
+    const testArea = this.target.querySelector(".test");
+    testArea.addEventListener("focus", () => {
+      this.focused = true;
     });
-    this.target.addEventListener("keyup", (e) => {
-      for(let action in this.actionsToBind){
-        if(this.actionsToBind[action].keys.includes(e.keyCode)){
-          this.disableAction(action)
-        }
-      }
+    testArea.addEventListener("blur", () => {
+      this.focused = false;
+    });
+
+    let buttonAttach = this.target.querySelector(".btnAttach");
+    buttonAttach.addEventListener("click", () => {
+      this.attach(this.target, !this.enabled);
+    });
+    let buttonDetach = this.target.querySelector(".btnDetach");
+    buttonDetach.addEventListener("click", () => {
+      this.detach();
+    });
+
+    let buttonActivate = this.target.querySelector(".btnActivate");
+    buttonActivate.addEventListener("click", () => {
+      this.enabled = true;
+    });
+    let buttonDeactivate = this.target.querySelector(".btnDeactivate");
+    buttonDeactivate.addEventListener("click", () => {
+      this.enabled = false;
+      this.detach();
     });
   }
+
+  keydownHandler = (e) => {
+    for (let action in this.actionsToBind) {
+      if (this.actionsToBind[action].keys.includes(e.keyCode)) {
+        this.enableAction(action);
+      }
+    }
+  };
+
+  keyupHandler = (e) => {
+    for (let action in this.actionsToBind) {
+      if (this.actionsToBind[action].keys.includes(e.keyCode)) {
+        this.disableAction(action);
+      }
+    }
+  };
 
   bindActions(actionsToBind) {
     this.actionsToBind = actionsToBind;
   }
 
   enableAction(actionName) {
-    this.actionsToBind[actionName].enabled=true;
-    // let e = new Event(this.ACTION_ACTIVATED);
-    // window.dispatchEvent(e)
-    let event = new Event(actionName)
-    window.dispatchEvent(event)
+    if (this.focused) {
+      this.actionsToBind[actionName].enabled = true;
+      if (this.isActionActive(actionName)) {
+        let event = new Event(actionName);
+        window.dispatchEvent(event);
+      }
+    }
   }
 
   disableAction(actionName) {
-    this.actionsToBind[actionName].enabled=false;
-    let event = new Event (this.ACTION_DEACTIVATED);
-    window.dispatchEvent(event)
+    this.actionsToBind[actionName].enabled = false;
+    let event = new Event(this.ACTION_DEACTIVATED);
+    window.dispatchEvent(event);
+  }
+
+  attach(target, dontUnable) {
+    if (dontUnable) {
+      return;
+    }
+    target.addEventListener("keydown", this.keydownHandler);
+    target.addEventListener("keyup", this.keyupHandler);
+  }
+
+  detach() {
+    this.target.removeEventListener("keydown", this.keydownHandler);
+    this.target.removeEventListener("keyup", this.keyupHandler);
   }
 
   isActionActive(action) {
-    if(this.actionsToBind[action].enabled === true){
-      return true
+    if (this.actionsToBind[action].enabled === true) {
+      return true;
+    } else {
+      return false;
     }
-    // for (let i = 0; i < this.actionsToBind[action].keys.length; i++) {
-    //   if (this.isKeyPressed(this.actionsToBind[action].keys[i])) {
-    //     return true;
-    //   }
-    // }
   }
 
   isKeyPressed(keyCode) {
